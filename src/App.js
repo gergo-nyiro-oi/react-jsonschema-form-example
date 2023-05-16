714caa68c59ab0a1c5c904ccbb364b94729aa282
Nyiro Gergo <gergo.nyiro@oneidentity.com>
2023-05-16 16:56:05 +0200

    src/App.js: load schema from file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,7 @@
 import './App.css';
 import validator from '@rjsf/validator-ajv8';
 import Form from '@rjsf/core';
-import { RJSFSchema } from '@rjsf/utils';
-
-
-const schema: RJSFSchema = {
-  title: 'Todo',
-  type: 'object',
-  required: ['title'],
-  properties: {
-    title: { type: 'string', title: 'Title', default: 'A new task' },
-    done: { type: 'boolean', title: 'Done?', default: false },
-  },
-};
+import schema from './schema.json';
 
 const log = (type) => console.log.bind(console, type);
 
