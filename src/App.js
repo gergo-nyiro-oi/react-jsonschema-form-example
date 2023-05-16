04c4902ac6cd082ac8ec32bf450192553d8db475
Nyiro Gergo <gergo.nyiro@oneidentity.com>
2023-05-16 15:35:08 +0200

    src/App.js: first example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import validator from '@rjsf/validator-ajv8';
+import Form from '@rjsf/core';
+import { RJSFSchema } from '@rjsf/utils';
+
+
+const schema: RJSFSchema = {
+  title: 'Todo',
+  type: 'object',
+  required: ['title'],
+  properties: {
+    title: { type: 'string', title: 'Title', default: 'A new task' },
+    done: { type: 'boolean', title: 'Done?', default: false },
+  },
+};
+
+const log = (type) => console.log.bind(console, type);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+      <Form
+        schema={schema}
+        validator={validator}
+        onChange={log('changed')}
+        onSubmit={log('submitted')}
+        onError={log('errors')}
+      />
+    );
 }
 
 export default App;
