4400d8d6883c8895fb4406cb9d3cbbca8c262e46
Nyiro Gergo <gergo.nyiro@oneidentity.com>
2023-05-23 16:42:47 +0200

    package.json: proxy

--- src/App.js
+++ src/App.js
@@ -2,10 +2,51 @@ import './App.css';
 import validator from '@rjsf/validator-ajv8';
 import Form from '@rjsf/core';
 import schema from './schema.json';
+import React, { useState, useEffect } from "react";
 
 const log = (type) => console.log.bind(console, type);
 
+
 function App() {
+    var path = window.location.pathname === "/" ? "/api" : window.location.pathname;
+
+    return (
+        <Navagation
+            path={path}
+        />
+    );
+}
+
+function Navagation( { path } ) {
+  const [ data, setData ] = useState({});
+
+  useEffect(() => {
+    fetch(path)
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        setData(json.meta);
+      });
+  }, [path]);
+
+  return (
+    <div>
+      <ul>
+        {
+            Object.keys(data).map(
+                (k) => (
+                    <li key={k}>
+                      <a href={data[k]}>{k}</a>
+                    </li>
+                )
+            )
+        }
+      </ul>
+    </div>
+  );
+}
+
+function FormApp() {
     return (
       <Form
         schema={schema}
@@ -15,6 +56,7 @@ function App() {
         onError={log('errors')}
       />
     );
+
 }
 
 export default App;
